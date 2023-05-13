086f0e07833f16a5d9d63391c4f19e3868b752ee
VuongTanPhat <133354234+VuongTanPhat@users.noreply.github.com>
2023-05-13 11:47:30 +0700

    Code index

--- src/main/java/com/demo/controller/HomeController.java
+++ src/main/java/com/demo/controller/HomeController.java
@@ -1,8 +1,15 @@
 package com.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class HomeController {
-	///hello Phương nek im here
+	@GetMapping("/index")
+	public String index(Model model) {
+		model.addAttribute("message", "Phat hahahahaha");
+		return "index";
+	}
 }
